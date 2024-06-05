18c620dc6dd8b67a4ac469570ed3fc715bb3662d
ButterMun <92117443+ButterMun@users.noreply.github.com>
2024-06-05 15:21:41 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,10 +3,10 @@ document.getElementById('ton-connect').style.display = 'none';
 
 async function connectToWallet() {
     const tonConnectUI = new TON_CONNECT_UI.TonConnectUI({
-        manifestUrl: 'https://buttermun.github.io/tonopoly/tonconnect-manifest.json',
+        manifestUrl: 'https://buttermun.github.io/investcoin/tonconnect-manifest.json',
         buttonRootId: 'ton-connect',
         uiOptions: {
-            twaReturnUrl: 'https://t.me/tonopolygame_bot'
+           // twaReturnUrl: 'https://t.me/tonopolygame_bot'
         }
     });
 
@@ -15,13 +15,32 @@ async function connectToWallet() {
         const accountAddress = connectedWallet.account.address;
 
         console.log("Wallet address:", accountAddress);
-        // После успешного подключения к кошельку, отправьте номер аккаунта на сервер
-        // и получите количество монет, если аккаунт уже существует
-        // При каждом клике на кнопку, обновите количество монет в базе данных MySQL
+       
+        // Обновление профиля
+        updateProfile(accountAddress);
     } catch (error) {
         console.error("Error connecting to wallet:", error);
+		window.location.reload();
+            
     }
 }
+
+function updateProfile(walletAddress) {
+    // Находим элемент с информацией о профиле
+    const profileInfo = document.querySelector('.profile-info');
+    
+    // Находим элемент с текстом "Status: Inactive" и заменяем его на "Status: Active"
+    const statusElement = profileInfo.querySelector(':nth-last-child(5)'); // Ищем предпоследний элемент
+    statusElement.textContent = "Status: Active";
+    
+    // Обновляем адрес кошелька 
+    const walletAddressElement = profileInfo.querySelector('p:last-child');
+    walletAddressElement.textContent = "Wallet Address: " + walletAddress.substring(0, 5) + '...' + walletAddress.substring(walletAddress.length - 5);
+
+}
+
+
+
 
 // Вызов функции подключения к кошельку при загрузке страницы
 connectToWallet();
